4b7b60d1d3fa7aca1f931011b46a7bfb4b5ce4b4
EDT-ELT-DEPARTMENT <milouafarid@gmail.com>
2026-07-30 01:00:19 +0100

    Update edt_app.py

--- edt_app.py
+++ edt_app.py
@@ -5861,7 +5861,7 @@ else:
     with c2:
         st.download_button("📄 Télécharger la vue (HTML)", df_pub.to_html(index=False), "EDT_Vue_Publique.html", "text/html")
 
-        elif portail == "📢 Gestion Administrative - Bordereaux & PVs":
+ elif portail == "📢 Gestion Administrative - Bordereaux & PVs":
             if not is_admin:
                 st.error("🚫 ACCÈS RESTREINT.")
                 st.stop()
@@ -6526,3 +6526,4 @@ else:
             st.divider()
             with st.expander("📜 Historique détaillé des bordereaux générés", expanded=False):
                 afficher_historique_bordereaux()
+       
