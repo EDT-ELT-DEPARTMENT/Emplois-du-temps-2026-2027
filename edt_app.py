988cb11b9417e2d1f949cdfebfbc5907cb275bd0
EDT-ELT-DEPARTMENT <milouafarid@gmail.com>
2026-08-09 16:02:49 +0100

    Update edt_app.py

--- edt_app.py
+++ edt_app.py
@@ -4023,7 +4023,7 @@ Cet email est généré automatiquement - merci de ne pas y répondre.
 # =============================================================================
 
 if module_sel == "📊 Suivi d'Assiduite":
-    run_Assiduite()
+    run_assiduite()
 else:
     run_edt() 
 
