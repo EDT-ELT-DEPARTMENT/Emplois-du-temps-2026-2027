958e6d3283dbb2057e820ac6592589ddb7f0650c
EDT-ELT-DEPARTMENT <milouafarid@gmail.com>
2026-08-12 10:01:52 +0100

    Update edt_app.py

--- edt_app.py
+++ edt_app.py
@@ -2895,7 +2895,7 @@ Cet email est généré automatiquement - merci de ne pas y répondre.
     if portail == "📖 Emploi du Temps" and is_admin:
         mode_view = st.radio("Vue Administration :", [
             "Promotion", "Enseignant", "🏢 Planning Salles", 
-            "🚩 Vérificateur de conflits", "🔗 Matières communes entre Promotions"
+            "🚩 Vérificateur de conflits"
         ], horizontal=True)
     
         poste_sup = st.checkbox("Poste Supérieur (Décharge 3h)")
